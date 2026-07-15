4254a181116e921d60c2d22446894f9b8467b2ab
Mithesh Satvilkar <mitheshsatvilkar@gmail.com>
2026-07-15 22:51:57 +0530

    Backend Database Changes

--- backend/database/mongoDB.js
+++ backend/database/mongoDB.js
@@ -2,19 +2,16 @@ const mongoose = require('mongoose');
 
 const connectDB =()=>{
     try{
-        console.log(process.env.DATABASE_LOCAL);
-        mongoose.connect(process.env.DATABASE_LOCAL)
+        mongoose.connect(process.env.DATABASE_MONGO.replace("<db_password>",process.env.MONGODB_PASS))
         .then(()=> {
             console.log("✅ MongoDB Connected Successfully");
             console.log(`📦 Database : ${process.env.DB_NAME}`);
-            console.log(`🖥️ Host      : ${process.env.DB_HOST}:${process.env.DB_PORT}`);
-
+            // console.log(`🖥️ Host      : ${process.env.DATABASE_MONGO}`);
         })
     }
     catch(err){
         console.error("❌ Database Connection Failed");
         console.error(err.message);
-
         process.exit(1);
     }
     
