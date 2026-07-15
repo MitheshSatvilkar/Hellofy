f96e03f2e09176e5ad9716599d6c05ddeb13c7f2
Mithesh Satvilkar <mitheshsatvilkar@gmail.com>
2026-07-16 00:39:02 +0530

    main.jsx changes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { GlobalProvider } from './Context/GlobalContext.jsx'
+import { GlobalProvider } from './context/GlobalContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
